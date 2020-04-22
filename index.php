c2889e9b1671ce3a28ff4c946f7fd2a438f3d6eb
Niccolò Predazzer <niccolo.predazzer@gmail.com>
2020-04-22 21:21:59 +0200

    start

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>adscharts</title>
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    </head>
+    <body>
+
+
+
+
+         <script src="js/main.js" charset="utf-8"></script>
+    </body>
+</html>
