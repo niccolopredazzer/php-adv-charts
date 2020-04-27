5bd38af9f3033e0ef13762cbe12de15c83394a80
Niccolò Predazzer <niccolo.predazzer@gmail.com>
2020-04-27 10:25:43 +0200

    2.

--- server.php
+++ server.php
@@ -1,7 +1,7 @@
 <?php
 
-    include 'database.php';
+    include 'data.php';
     header('Content-Type: application/json');
     echo json_encode($data);
-    
+
 ?>
--- server2.php
+++ server2.php
@@ -1,6 +1,6 @@
 <?php
 
-    include 'data.php';
+    include 'data2.php';
     header('Content-Type: application/json');
     echo json_encode($graphs);
 
